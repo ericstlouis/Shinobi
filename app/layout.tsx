364ec780c0963abc51e2718538351fca5340e3a0
Eric <ericstlouis2001@gmail.com>
2023-03-02 23:44:04 -0500

    viewport fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='font.className'>
+    <html lang="en" className='font.className max-h-screen'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
